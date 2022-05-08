7d6ff8d1839773c3d9ce5de82f70aa0ef7ecefb6
Farid Rehila <farid.rehila@gmail.com>
2022-05-08 03:21:27 +0200

    react back

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const formidableMiddleware = require("express-formidable");
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const app = express();
+app.use(formidableMiddleware());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const userRoutes = require("./Routes/user");
+app.use(userRoutes);
+
+const offerRoutes = require("./Routes/offer");
+app.use(offerRoutes);
+
+app.get("*", (req, res) => {
+  res.json("Page introuvable");
+});
+
+app.listen(3000, () => {
+  console.log("Server launched ! 🚀");
+});
