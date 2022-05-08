9a5f1562ef6372366417bd8dd3ba4ee232b95160
Farid Rehila <farid.rehila@gmail.com>
2022-05-08 03:58:03 +0200

    T_T

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("*", (req, res) => {
   res.json("Page introuvable");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
   console.log("Server launched ! 🚀");
 });
